e6888fa5a5e8a483cfab5d435f468b0f3a86b9bc
Andrea Parthemore <aparthemore@vistahigherlearning.com>
2015-08-24 18:45:31 -0400

    passenger-status-check: #report method builds a report object.

--- lib/passenger_status_check/parser.rb
+++ lib/passenger_status_check/parser.rb
@@ -1,5 +1,87 @@
+require 'ox'
+require 'ostruct'
+
 module PassengerStatusCheck
   class Parser
+    attr_reader :xml
+
+    def initialize(file_path)
+      @data = File.read(file_path)
+    end
+
+    def parse
+      @xml = Ox.parse(@data)
+    end
+
+    # build_report returns an object with passenger status check stats
+    # { requests_in_top_level_queue: 'abc',
+    #   requests_in_queue: 'abc',
+    #   processes: [{ memory: '123', cpu: '123', last_time_request_handled: '123'},
+    #               { memory: '123', cpu: '123', last_time_request_handled: '123'}
+    #              ]
+    # }
+    #
+
+    def build_report
+      report = OpenStruct.new()
+      top_level_report_items = ['requests_in_top_level_queue', 'requests_in_queue']
+      process_level_report_items = ['memory', 'cpu', 'last_time_request_handled']
+      top_level_report_items.each do |item|
+        # Call the method with the name of item
+        # Assign the result to an oject key with the name of item
+        report[item.to_s] = self.send(item)
+      end
+
+      # Create an array to hold process reporting
+      report.processes = []
+
+      # process_list is an ox object containing all of the process nodes
+      process_list = @xml.locate("info/supergroups/supergroup/group/processes/process/")
+
+      process_list.each_with_index {|item, index|
+        # For each process node, add a new object to the processes key of the report object
+        report.processes.push(OpenStruct.new())
+
+        # Add stats to the process object
+        process_level_report_items.each do |process_item|
+          # Call the method with the name of process_item
+          # Assign the result to an oject key with the name of process_item
+          report.processes[index][process_item.to_s] =  self.send(process_item, item)
+        end
+
+        # I think I might like this simple code below better...
+        # report.processes[index].memory = item.locate('rss/*').first
+        # report.processes[index].cpu = item.locate('cpu/*').first
+        # report.processes[index].last_time_request_handled = item.locate('last_used/*').first
+      }
+      report
+    end
+
+    def requests_in_top_level_queue
+      @xml.locate("info/get_wait_list_size/*").first
+    end
+    private :requests_in_top_level_queue
+
+    def requests_in_queue
+      @xml.locate("info/supergroups/supergroup/group/get_wait_list_size/*").first
+    end
+    private :requests_in_queue
+
+    def memory(item)
+      item.locate('rss/*').first
+    end
+    private :memory
+
+    def cpu(item)
+      item.locate('cpu/*').first
+    end
+    private :cpu
+
+    def last_time_request_handled(item)
+      item.locate('last_used/*').first
+    end
+    private :last_time_request_handled
 
   end
+
 end
--- spec/lib/passenger_status_check/parser_spec.rb
+++ spec/lib/passenger_status_check/parser_spec.rb
@@ -1,9 +1,33 @@
 require 'spec_helper'
+require 'byebug'
 
 describe PassengerStatusCheck::Parser do
 
-  it 'returns true' do
-    expect(true).to be_truthy
+  let(:status_check) do
+    PassengerStatusCheck::Parser.new('spec/fixtures/pass-status.xml')
   end
 
+  before(:each) do
+    status_check.parse
+    @report = status_check.build_report
+  end
+
+  describe '#parse' do
+    it 'loads the XML file' do
+      expect(status_check.xml['version']).to eql('1.0')
+    end
+  end
+
+  describe '#report' do
+    it 'builds a report from the xml' do
+      expect(@report.requests_in_top_level_queue).to eq('0')
+      expect(@report.requests_in_queue).to eq('8')
+    end
+
+    it 'reports the process information' do
+      expect(@report.processes.first.memory).to eq('264508')
+      expect(@report.processes.first.cpu).to eq('0')
+      expect(@report.processes.first.last_time_request_handled).to eq('1440404826866466')
+    end
+  end
 end
