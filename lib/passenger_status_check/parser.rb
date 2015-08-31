89843c05cc156d1a131c75cb784f2652bd44e350
Keith Morrison <kleonardmorrison@gmail.com>
2015-08-31 12:30:55 -0400

    Continue refactoring.

--- lib/passenger_status_check/parser.rb
+++ lib/passenger_status_check/parser.rb
@@ -9,69 +9,26 @@ module PassengerStatusCheck
       @data = File.read(file_path)
     end
 
-    def parse
-      @xml = Ox.parse(@data)
+    def xml
+      @xml ||= Ox.parse(@data)
     end
 
-    # build_report returns an object with passenger status check stats
-    # { requests_in_top_level_queue: 'abc',
-    #   requests_in_queue: 'abc',
-    #   processes: [{ memory: '123', cpu: '123', last_time_request_handled: '123'},
-    #               { memory: '123', cpu: '123', last_time_request_handled: '123'}
-    #              ]
-    # }
-    #
-
-    def build_report
-      report = OpenStruct.new()
-      top_level_report_items = ['requests_in_top_level_queue', 'requests_in_queue']
-      process_level_report_items = ['memory', 'cpu', 'last_time_request_handled']
-      top_level_report_items.each do |item|
-        # Call the method with the name of item
-        # Assign the result to an oject key with the name of item
-        report[item.to_s] = self.send(item)
-      end
-
-      # Create an array to hold process reporting
-      report.processes = []
-
-      report.processes = @xml.locate("info/supergroups/supergroup/group/processes/process/").map do |process_xml|
+    def processes
+      @processes ||= xml.locate("info/supergroups/supergroup/group/processes/process/").map do |process_xml|
         Process.new(process_xml)
       end
-
-      # process_list.each_with_index {|item, index|
-      #   # For each process node, add a new object to the processes key of the report object
-      #   report.processes.push(OpenStruct.new())
-
-      #   # Add stats to the process object
-      #   process_level_report_items.each do |process_item|
-      #     # Call the method with the name of process_item
-      #     # Assign the result to an oject key with the name of process_item
-      #     # report.processes[index][process_item.to_s] =  self.send(process_item, item)
-      #   end
-
-      #   # I think I might like this simple code below better...
-      #   # report.processes[index].memory = item.locate('rss/*').first
-      #   # report.processes[index].cpu = item.locate('cpu/*').first
-      #   # report.processes[index].last_time_request_handled = item.locate('last_used/*').first
-      # }
-      report
     end
 
     def process_count
-      @xml.locate('info/supergroups/supergroup/group/processes/process').size
+      xml.locate('info/supergroups/supergroup/group/processes/process').size
     end
 
     def requests_in_top_level_queue
-      @xml.locate("info/get_wait_list_size/*").first
+      xml.locate("info/get_wait_list_size/*").first
     end
-    private :requests_in_top_level_queue
 
-    def requests_in_queue
-      @xml.locate("info/supergroups/supergroup/group/get_wait_list_size/*").first
+    def requests_in_app_queue
+      xml.locate("info/supergroups/supergroup/group/get_wait_list_size/*").first
     end
-    private :requests_in_queue
-
   end
-
 end
--- spec/lib/passenger_status_check/parser_spec.rb
+++ spec/lib/passenger_status_check/parser_spec.rb
@@ -8,8 +8,6 @@ describe PassengerStatusCheck::Parser do
 
   before(:each) do
     @parser = status_check
-    status_check.parse
-    @report = status_check.build_report
   end
 
   describe '#parse' do
@@ -18,15 +16,33 @@ describe PassengerStatusCheck::Parser do
     end
   end
 
-  describe '#report' do
-    it 'builds a report from the xml' do
-      expect(@report.requests_in_top_level_queue).to eq('0')
-      expect(@report.requests_in_queue).to eq('8')
+  describe '#process_count' do
+    it 'returns the number of active processes' do
+      expect(@parser.process_count).to eq(2)
+    end
+  end
+
+  describe '#requests_in_top_level_queue' do
+    it 'returns the number of requests in the global queue' do
+      expect(@parser.requests_in_top_level_queue).to eq('0')
+    end
+  end
+
+  describe '#requests_in_app_queue' do
+    it 'returns the number of requests backed up in the app queue' do
+      expect(@parser.requests_in_app_queue).to eq('8')
+    end
+  end
+
+
+  describe '#processes' do
+    it 'contains the correct number of processes' do
+      expect(@parser.processes.size).to eq(2)
     end
 
     context 'given a process' do
       before do
-        @process = @report.processes.first
+        @process = @parser.processes.first
       end
 
       it 'reports the cpu usage' do
@@ -39,12 +55,6 @@ describe PassengerStatusCheck::Parser do
 
       it 'reports the last request time' do
         expect(@process.last_request_time).to eq('1440404826866466')
-      end
-    end
-
-    context 'given active processes' do
-      it 'returns the number of processes' do
-        expect(@parser.process_count).to eq(2)
       end
     end
   end
