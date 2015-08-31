a1b35b896c14703705ad0622c2d113dba370d8ba
Keith Morrison <kleonardmorrison@gmail.com>
2015-08-31 15:54:22 -0400

    Define a 'run' method on the main module.

--- lib/passenger_status_check.rb
+++ lib/passenger_status_check.rb
@@ -1,7 +1,18 @@
 require 'ox'
 require "passenger_status_check/version"
 require 'passenger_status_check/parser'
+require 'passenger_status_check/formatters/check_mk'
 
 module PassengerStatusCheck
-  # Your code goes here...
+
+  def self.run(formatter_name, thresholds, data)
+    parser = PassengerStatusCheck::Parser.new(data)
+    formatter = formatter_class(formatter_name).new(parser, thresholds)
+    formatter.output
+  end
+
+  def self.formatter_class(formatter)
+    klass = formatter.to_s.split('_').map(&:capitalize).join
+    const_get("PassengerStatusCheck::Formatters::#{klass}")
+  end
 end
--- spec/passenger_status_check_spec.rb
+++ spec/passenger_status_check_spec.rb
@@ -5,7 +5,29 @@ describe PassengerStatusCheck do
     expect(PassengerStatusCheck::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+
+  describe '.run' do
+    before do
+      file_path = 'spec/fixtures/pass-status.xml'
+      @data = File.read(file_path)
+    end
+
+    it 'generates output compatible with the specified agent' do
+
+      expected = <<-TXT
+0 Global_queue count=0 Global queue: 0
+2 Application_queue count=8 Application queue: 8
+0 Passenger_processes count=2 Passenger processes: 2
+0 Passenger_8439 cpu=0|memory=264508|last_request_time=1440404826866466 Passenger pid 8439 cpu:0 memory:264508 last_request_time:1440404826866466
+0 Passenger_8449 cpu=2|memory=439276|last_request_time=1440424552033587 Passenger pid 8449 cpu:2 memory:439276 last_request_time:1440424552033587
+      TXT
+
+      formatter  = :check_mk
+      thresholds = { gqueue: 0, aqueue: 0, pcount: 2..2 }
+
+      output = PassengerStatusCheck.run(formatter, thresholds, @data)
+
+      expect(output).to match(expected)
+    end
   end
 end
