c9300da3762e2f3c7056cddc4fbab0f9872fa4c8
Sheilah Hoelscher <shoelscher@vistahigherlearning.com>
2016-02-18 09:52:08 -0500

    Update all tests to cover resisting deployment error check.

--- spec/passenger_status_check_spec.rb
+++ spec/passenger_status_check_spec.rb
@@ -20,6 +20,31 @@ describe PassengerStatusCheck do
 0 Passenger_processes count=2 Passenger processes: 2
 0 Passenger_8439 cpu=0|memory=264508|last_request_time=1440404826866466 Passenger pid 8439 cpu:0 memory:264508 last_request_time:1440404826866466
 0 Passenger_8449 cpu=2|memory=439276|last_request_time=1440424552033587 Passenger pid 8449 cpu:2 memory:439276 last_request_time:1440424552033587
+0 Passenger_resisting_deployment_status - OK
+      TXT
+
+      formatter  = :check_mk
+      thresholds = { gqueue: 0, aqueue: 0, pcount: 2..2 }
+
+      output = PassengerStatusCheck.run(formatter, thresholds, @data)
+
+      expect(output).to match(expected)
+    end
+  end
+  describe 'error .run' do
+    before do
+      file_path = 'spec/fixtures/pass-status-error.xml'
+      @data = File.read(file_path)
+    end
+
+    it 'generates error output compatible with the specified agent' do
+
+      expected = <<-TXT
+0 Global_queue count=0 Global queue: 0
+2 Application_queue count=14 Application queue: 14
+2 Passenger_processes count=1 Passenger processes: 1
+0 Passenger_workers p0_cpu=7|p0_memory=311936|p0_last_request_time=1454007475374106 Passenger worker details
+2 Passenger_resisting_deployment_status - CRITICAL
       TXT
 
       formatter  = :check_mk
