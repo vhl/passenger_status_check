985d6ea00f2eb57018d6d5d6a5d689f5f5373ad5
Jorge Orlando Muñoz <jmunoz@altavistaed.com>
2016-07-05 15:05:45 -0500

    Add DogStatsd formatter.

---
+++ lib/passenger_status_check/formatters/dog_statsd.rb
@@ -0,0 +1,62 @@
+# coding: utf-8
+#
+# Copyright © 2015 Vista Higher Learning, Inc.
+#
+# This file is part of passenger_status_check.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'passenger_status_check/checks/passenger_check'
+require 'statsd'
+
+module PassengerStatusCheck
+  module Formatters
+    class DogStatsd
+      attr_reader :parser
+      #STATUS_LOOKUP = { 0 => 'OK', 1 => 'WARNING', 2 => 'CRITICAL', 3 => 'UNKNOWN' }.freeze
+
+      def initialize(parser, thresholds)
+        @parser = parser
+        @thresholds = thresholds
+        # Statsd instance should be created, if possible, once for the duration of the process
+        @statsd = Statsd.new('localhost', 8125)
+      end
+
+      def passenger_check
+        @passenger_check ||= PassengerStatusCheck::Checks::PassengerCheck.new(@parser, @thresholds)
+      end
+
+      def output
+        @statsd.batch do |stats|
+         stats.histogram('passenger.global_queue.count', parser.requests_in_top_level_queue, tags: tags)
+         stats.histogram('passenger.application_queue.count', parser.requests_in_app_queue, tags: tags)
+         stats.histogram('passenger.processes.count', parser.process_count, tags: tags)
+         parser.processes.map.with_index do |process, i|
+           stats.histogram("passenger.process_#{i}.cpu", process.cpu, tags: tags)
+           stats.histogram("passenger.process_#{i}.memory", process.memory, tags: tags)
+           stats.histogram("passenger.process_#{i}.last_request_time", process.last_request_time, tags: tags)
+         end
+         status = passenger_check.resisting_deployment_check
+         stats.event('passenger.resisting_deployment_status', CheckMk::STATUS_LOOKUP[status], tags: tags)
+        end
+      end
+
+      def tags
+        # Currently it has not been defined how the tags on Datadog will be setup.
+        ['testing', 'to_be_defined']
+      end
+      private :tags
+    end
+  end
+end
---
+++ spec/lib/passenger_status_check/formatters/dog_statsd_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'passenger_status_check/formatters/dog_statsd'
+
+describe PassengerStatusCheck::Formatters::DogStatsd do
+  describe '#format' do
+    let(:statsd) { double(Statsd).as_null_object }
+    let(:data) { File.read('spec/fixtures/pass-status.xml') }
+    let(:parser) { PassengerStatusCheck::Parser.new(data) }
+    let(:thresholds) { { gqueue: 0, aqueue: 0, pcount: 2..2 } }
+    let(:formatter) { described_class.new(parser, thresholds) }
+    let(:tags) { ['testing'] }
+
+    before do
+      allow(statsd).to receive(:batch).and_yield(statsd)
+      allow(Statsd).to receive(:new).and_return(statsd)
+      allow(formatter).to receive(:tags).and_return(tags)
+    end
+
+    it 'sends global queue count metrics' do
+      expect(statsd).to receive(:histogram).with('passenger.global_queue.count', 0, tags: tags)
+      formatter.output
+    end
+
+    it 'sends application queue count metrics' do
+      expect(statsd).to receive(:histogram).with('passenger.application_queue.count', 8, tags: tags)
+      formatter.output
+    end
+
+    it 'sends passenger process count metrics' do
+      expect(statsd).to receive(:histogram).with('passenger.processes.count', 2, tags: tags)
+      formatter.output
+    end
+
+    it 'sends passenger workers stats' do
+      expect(statsd).to receive(:histogram).with("passenger.process_0.cpu", '0', tags: tags)
+      expect(statsd).to receive(:histogram).with("passenger.process_0.memory", '264508', tags: tags)
+      expect(statsd).to receive(:histogram).with("passenger.process_0.last_request_time", '1440404826866466', tags: tags)
+      expect(statsd).to receive(:histogram).with("passenger.process_1.cpu", '2', tags: tags)
+      expect(statsd).to receive(:histogram).with("passenger.process_1.memory", '439276', tags: tags)
+      expect(statsd).to receive(:histogram).with("passenger.process_1.last_request_time", '1440424552033587', tags: tags)
+      formatter.output
+    end
+
+    it 'sends passenger resisting deployment stats' do
+      expect(statsd).to receive(:event).with('passenger.resisting_deployment_status', 'OK', tags: tags)
+      formatter.output
+    end
+  end
+end
